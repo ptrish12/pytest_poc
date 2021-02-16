1964ddda0def5a0e9ab662aed0a50f5328bd884b
ptrish12 <pstichnoth@gmail.com>
2021-02-15 22:44:22 -0500

    initial commit

---
+++ dummy_functions.py
@@ -0,0 +1,8 @@
+
+
+def super_add(a, b):
+    return a**2 + b**2
+
+
+def super_multiply(a, b):
+    return a**2 * b**2
---
+++ tests.py
@@ -0,0 +1,10 @@
+import pytest
+from dummy_functions import super_add, super_multiply
+
+
+def test_add():
+    assert super_add(2,3) == 13
+
+
+def test_multiply():
+    assert super_multiply(2, 3) == 36
